2834e1978ae151d1904dde5f0a37c1ab00514836
Kady529 <kadiatoubah.enabel@outlook.com>
2021-08-02 15:10:36 +0000

    menu

--- resources/views/theme/menu.blade.php
+++ resources/views/theme/menu.blade.php
@@ -14,30 +14,30 @@
 
     <link href="https://fonts.googleapis.com/css2?family=Cormorant+Garamond:wght@300&display=swap" rel="stylesheet">
     <!-- Bootstrap -->
-    <link href="{{ asset('theme/vendors/bootstrap/dist/css/bootstrap.min.css')}}" rel="stylesheet">
+    <link href="{{ secure_asset('theme/vendors/bootstrap/dist/css/bootstrap.min.css')}}" rel="stylesheet">
     <!-- Font Awesome -->
-    <link href="{{ asset('theme/vendors/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
+    <link href="{{ secure_asset('theme/vendors/font-awesome/css/font-awesome.min.css')}}" rel="stylesheet">
     <!-- NProgress -->
-    <link href="{{ asset('theme/vendors/nprogress/nprogress.css')}}" rel="stylesheet">
+    <link href="{{ secure_asset('theme/vendors/nprogress/nprogress.css')}}" rel="stylesheet">
     <!-- iCheck -->
-    <link href="{{ asset('theme/vendors/iCheck/skins/flat/green.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('theme/vendors/iCheck/skins/flat/green.css') }}" rel="stylesheet">
     <!-- bootstrap-progressbar -->
-    <link href="{{ asset('theme/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('theme/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css') }}" rel="stylesheet">
     <!-- JQVMap -->
-    <link href="{{ asset('theme/vendors/jqvmap/dist/jqvmap.min.css')}}" rel="stylesheet"/>
+    <link href="{{ secure_asset('theme/vendors/jqvmap/dist/jqvmap.min.css')}}" rel="stylesheet"/>
     <!-- bootstrap-daterangepicker -->
-    <link href="{{ asset('theme/vendors/bootstrap-daterangepicker/daterangepicker.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('theme/vendors/bootstrap-daterangepicker/daterangepicker.css') }}" rel="stylesheet">
     <!-- Custom Theme Style -->
-    <link href="{{ asset('theme/build/css/custom.min.css') }}" rel="stylesheet">
+    <link href="{{ secure_asset('theme/build/css/custom.min.css') }}" rel="stylesheet">
     <!-- Ion.RangeSlider -->
-    <link href="{{ asset('theme/vendors/normalize-css/normalize.css')}}" rel="stylesheet">
-    <link href="{{ asset('theme/vendors/ion.rangeSlider/css/ion.rangeSlider.css')}}" rel="stylesheet">
-    <link href="{{ asset('theme/vendors/ion.rangeSlider/css/ion.rangeSlider.skinFlat.css')}}" rel="stylesheet">
-    <link href="{{ asset('theme/ion.rangeSlider/css/ion.rangeSlider.css')}}" rel="stylesheet">
+    <link href="{{ secure_asset('theme/vendors/normalize-css/normalize.css')}}" rel="stylesheet">
+    <link href="{{ secure_asset('theme/vendors/ion.rangeSlider/css/ion.rangeSlider.css')}}" rel="stylesheet">
+    <link href="{{ secure_asset('theme/vendors/ion.rangeSlider/css/ion.rangeSlider.skinFlat.css')}}" rel="stylesheet">
+    <link href="{{ secure_asset('theme/ion.rangeSlider/css/ion.rangeSlider.css')}}" rel="stylesheet">
     <!-- Bootstrap Colorpicker -->
-    <link href="{{ asset('theme/vendors/widget.css')}}" rel="stylesheet">
-    <link href="{{ asset('theme/vendors/theme.css')}}" rel="stylesheet">
-    <link rel="stylesheet" href="{{ asset('theme/vendors/rSlider.min.css')}}">
+    <link href="{{ secure_asset('theme/vendors/widget.css')}}" rel="stylesheet">
+    <link href="{{ secure_asset('theme/vendors/theme.css')}}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ secure_asset('theme/vendors/rSlider.min.css')}}">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
 </head>
@@ -142,66 +142,66 @@
     </div>
 
     <!-- jQuery -->
-    <script src="{{ asset('theme/vendors/jquery/dist/jquery.min.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/jquery/dist/jquery.min.js')}}"></script>
     <!-- Bootstrap -->
-    <script src="{{ asset('theme/vendors/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/bootstrap/dist/js/bootstrap.bundle.min.js')}}"></script>
     <!-- FastClick -->
-    <script src="{{ asset('theme/vendors/fastclick/lib/fastclick.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/fastclick/lib/fastclick.js')}}"></script>
     <!-- NProgress -->
-    <script src="{{ asset('theme/vendors/nprogress/nprogress.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/nprogress/nprogress.js')}}"></script>
     <!-- Chart.js')}} -->
-    <script src="{{ asset('theme/vendors/Chart.js/dist/Chart.min.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/Chart.js/dist/Chart.min.js')}}"></script>
     <!-- gauge.js')}} -->
-    <script src="{{ asset('theme/vendors/gauge.js/dist/gauge.min.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/gauge.js/dist/gauge.min.js')}}"></script>
     <!-- bootstrap-progressbar -->
-    <script src="{{ asset('theme/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js')}}"></script>
     <!-- iCheck -->
-    <script src="{{ asset('theme/vendors/iCheck/icheck.min.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/iCheck/icheck.min.js')}}"></script>
     <!-- Skycons -->
-    <script src="{{ asset('theme/vendors/skycons/skycons.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/skycons/skycons.js')}}"></script>
     <!-- Flot -->
-    <script src="{{ asset('theme/vendors/Flot/jquery.flot.js')}}"></script>
-    <script src="{{ asset('theme/vendors/Flot/jquery.flot.pie.js')}}"></script>
-    <script src="{{ asset('theme/vendors/Flot/jquery.flot.time.js')}}"></script>
-    <script src="{{ asset('theme/vendors/Flot/jquery.flot.stack.js')}}"></script>
-    <script src="{{ asset('theme/vendors/Flot/jquery.flot.resize.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/Flot/jquery.flot.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/Flot/jquery.flot.pie.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/Flot/jquery.flot.time.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/Flot/jquery.flot.stack.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/Flot/jquery.flot.resize.js')}}"></script>
     <!-- Flot plugins -->
-    <script src="{{ asset('theme/vendors/flot.orderbars/js/jquery.flot.orderBars.js')}}"></script>
-    <script src="{{ asset('theme/vendors/flot-spline/js/jquery.flot.spline.min.js')}}"></script>
-    <script src="{{ asset('theme/vendors/flot.curvedlines/curvedLines.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/flot.orderbars/js/jquery.flot.orderBars.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/flot-spline/js/jquery.flot.spline.min.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/flot.curvedlines/curvedLines.js')}}"></script>
     <!-- DateJS -->
-    <script src="{{ asset('theme/vendors/DateJS/build/date.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/DateJS/build/date.js')}}"></script>
     <!-- JQVMap -->
-    <script src="{{ asset('theme/vendors/jqvmap/dist/jquery.vmap.js')}}"></script>
-    <script src="{{ asset('theme/vendors/jqvmap/dist/maps/jquery.vmap.world.js')}}"></script>
-    <script src="{{ asset('theme/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/jqvmap/dist/jquery.vmap.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/jqvmap/dist/maps/jquery.vmap.world.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js')}}"></script>
     <!-- bootstrap-daterangepicker -->
-    <script src="{{ asset('theme/vendors/moment/min/moment.min.js')}}"></script>
-    <script src="{{ asset('theme/vendors/bootstrap-daterangepicker/daterangepicker.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/moment/min/moment.min.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/bootstrap-daterangepicker/daterangepicker.js')}}"></script>
     <script src="{{asset('theme/vendors/datatables.net-buttons/js/dataTables.buttons.min.js')}}"></script>
 
     <!-- Custom Theme Scripts -->
-    <script src="{{ asset('theme/build/js/custom.min.js')}}"></script>
+    <script src="{{ secure_asset('theme/build/js/custom.min.js')}}"></script>
 
-    <script src="{{ asset('theme/vendors/datatables.net/js/jquery.dataTables.min.js')}}"></script>
-    <script src="{{ asset('theme/vendors/datatables.net-bs/js/dataTables.bootstrap.min.js')}}"></script>
-    <script src="{{ asset('theme/vendors/datatables.net-buttons/js/dataTables.buttons.min.js')}}"></script>
-    <script src="{{ asset('theme/vendors/datatables.net-buttons-bs/js/buttons.bootstrap.min.js')}}"></script>
-    <script src="{{ asset('theme/vendors/datatables.net-buttons/js/buttons.flash.min.js')}}"></script>
-    <script src="{{ asset('theme/vendors/datatables.net-buttons/js/buttons.html5.min.js')}}"></script>
-    <script src="{{ asset('theme/vendors/datatables.net-buttons/js/buttons.print.min.js')}}"></script>
-    <script src="{{ asset('theme/vendors/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js')}}"></script>
-    <script src="{{ asset('theme/vendors/datatables.net-keytable/js/dataTables.keyTable.min.js')}}"></script>
-    <script src="{{ asset('theme/vendors/datatables.net-responsive/js/dataTables.responsive.min.js')}}"></script>
-    <script src="{{ asset('theme/vendors/datatables.net-responsive-bs/js/responsive.bootstrap.js')}}"></script>
-    <script src="{{ asset('theme/vendors/datatables.net-scroller/js/dataTables.scroller.min.js')}}"></script>
-    <script src="{{ asset('theme/vendors/jszip/dist/jszip.min.js')}}"></script>
-    <script src="{{ asset('theme/vendors/pdfmake/build/pdfmake.min.js')}}"></script>
-    <script src="{{ asset('theme/vendors/pdfmake/build/vfs_fonts.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/datatables.net/js/jquery.dataTables.min.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/datatables.net-bs/js/dataTables.bootstrap.min.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/datatables.net-buttons/js/dataTables.buttons.min.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/datatables.net-buttons-bs/js/buttons.bootstrap.min.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/datatables.net-buttons/js/buttons.flash.min.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/datatables.net-buttons/js/buttons.html5.min.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/datatables.net-buttons/js/buttons.print.min.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/datatables.net-keytable/js/dataTables.keyTable.min.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/datatables.net-responsive/js/dataTables.responsive.min.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/datatables.net-responsive-bs/js/responsive.bootstrap.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/datatables.net-scroller/js/dataTables.scroller.min.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/jszip/dist/jszip.min.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/pdfmake/build/pdfmake.min.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/pdfmake/build/vfs_fonts.js')}}"></script>
     <!-- Ion.RangeSlider -->
-    <script src="{{ asset('theme/ion.rangeSlider/js/ion.rangeSlider.js')}}"></script>
-    <script src="{{ asset('theme/vendors/widget.js')}}"></script>
-    <script src="{{ asset('theme/vendors/rSlider.min.js')}}"></script>
+    <script src="{{ secure_asset('theme/ion.rangeSlider/js/ion.rangeSlider.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/widget.js')}}"></script>
+    <script src="{{ secure_asset('theme/vendors/rSlider.min.js')}}"></script>
 
 
 
